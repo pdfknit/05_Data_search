177ffb37444eb3dd64a5406a5dc85a795b62ac7f
Sonya Y <yasenkovasofia@gmail.com>
2021-09-27 22:13:34 +0300

    lesson_02_online

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ url = 'http://www.google.ru'
 response = requests.get(url)
 soup = bs(response.text, 'html.parser')
 
+tag_a = soup.find('a')
+div_d = soup.find('div', attrs={'id': 'd'}) #кладем аттрибуты внутри, id, class, href...
+all_p = div_d.findChildren(recursive=False)
+select_p = soup.find_all('p', attrs={'class': 'red'})
 print(response)
