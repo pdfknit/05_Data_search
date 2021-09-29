82dc3e0e04f20b022b6feca6823a365fae1ae3b3
Sonya Y <yasenkovasofia@gmail.com>
2021-09-29 11:54:00 +0300

    lesson_01_dz

--- lesson_01/dz_01.py
+++ lesson_01/dz_01.py
@@ -1,19 +1,20 @@
-#1. Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для конкретного пользователя, сохранить JSON-вывод в файле *.json.
+#1. Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для конкретного пользователя,
+# сохранить JSON-вывод в файле *.json.
 
 import requests
 from pprint import pprint
-city = 'moscow'
+import json
+
 my_params = {
     'login': 'pdfknit',
-
+    'per page': 10
 }
 
-url = "https://api.github.com/users/defunkt"
-
-for i in range(500):
-    response = requests.get(url, params=my_params)
-    j_data = response.json()
-    pprint(f'Р’ РіРѕСЂРѕРґРµ {j_data.get("name")} + {i} С‚РµРјРїРµСЂР°С‚СѓСЂР° {j_data.get("main").get("temp") - 273.15} РіСЂР°РґСѓСЃРѕРІ')
+url = "https://api.github.com/users/pdfknit/repos"
 
 
+response = requests.get(url, params=my_params)
+j_data = response.json()
 
+with open('dz_01.txt', 'w') as f:
+  json.dump(j_data, f, ensure_ascii=False, indent=4, sort_keys=True)
